aac06fe884269ae4a8b0ffa46c38205ffd954e9f
Lima-49 <vitorlima.soares@hotmail.com>
2023-11-15 23:54:39 -0300

    atualizacao

--- src/main/java/com/project/educacaogamificada/EducacaoGamificadaApplication.java
+++ src/main/java/com/project/educacaogamificada/EducacaoGamificadaApplication.java
@@ -3,9 +3,11 @@ package com.project.educacaogamificada;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Controller
 @SpringBootApplication
 public class EducacaoGamificadaApplication {
 
